e0cbf1e0aeba52f24978da69002998cb96efcbbf
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-02-28 22:30:57 -0600

    Added custom font and colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      poppins: ["Poppins"],
+    },
+    colors: {
+      dark_blue: "#125276",
+      dark_purple: "#431766",
+      lighter_purple: "#741476",
+      white: "#ffffff",
+    },
   },
   variants: {},
   plugins: [],
