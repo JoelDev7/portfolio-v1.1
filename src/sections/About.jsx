5b0f4ea08b3c4552f8a9f5b84923ad3dabda2d8b
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-03-02 14:28:11 -0600

    Created Hero and About sections.

---
+++ src/sections/About.jsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <section className="w-3/5 mx-auto pb-[30vh]">
+      <h2 className="text-[28px] font-bold uppercase text-center mb-5">
+        About
+      </h2>
+      <p className="text-[24px] font-light leading-[30pt] mb-[30pt]">
+        I’m passionate about creating intuitive and visually appealing user
+        experiences, design is another of my main interest, and I’m working on
+        expanding my knowledge to add it as a complementary skill.
+      </p>
+      <p className="text-[24px] font-light">
+        My goal is to build applications that besides looking good, also have
+        great performance and scalability.
+      </p>
+    </section>
+  );
+}
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,12 +1,12 @@
 export default function Hero() {
   return (
-    <div className="flex items-center justify-center w-full h-full">
+    <section className="flex items-center justify-center w-full h-screen">
       <h1 className="text-[114px] uppercase text-center font-semibold leading-[75px]">
         Joel Balderas <br />
         <span className="text-[40px] font-light bg-white bg-opacity-20 px-[10px] py-1 rounded-[5px]">
           Front end developer & design enthusiast
         </span>
       </h1>
-    </div>
+    </section>
   );
 }
