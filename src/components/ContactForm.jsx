ee21b61e8a96a0beb132efc27224e53c2b95c0a1
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-03-03 15:26:28 -0600

    Imported input and text area components.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,6 @@
 import FormInput from "../components/FormInput";
+import FormButton from "./FormButton";
+import FormTextarea from "./FormTextarea";
 
 export default function ContactForm() {
   const formFields = {
@@ -10,12 +12,19 @@ export default function ContactForm() {
     textarea: [{ title: "Message" }],
   };
   return (
-    <form className="w-6/12 h-[80vh] mx-auto bg-white bg-opacity-[15%] rounded p-16">
+    <form className="w-6/12 min-h-[80vh] mx-auto bg-white bg-opacity-[15%] rounded p-16">
       <div className="grid grid-cols-2 gap-x-10">
         {formFields.inputs.map((input) => {
-          const inputId = input.title.replace(/ /g, "_");
+          const inputId = input.title.replace(/ /g, "_").toLowerCase();
           return <FormInput {...input} id={inputId} key={inputId} />;
         })}
+        <FormTextarea
+          title={formFields.textarea[0].title}
+          id={formFields.textarea[0].title.toLowerCase()}
+        />
+      </div>
+      <div className="w-24 mx-auto">
+        <FormButton title={"Send"} />
       </div>
     </form>
   );
