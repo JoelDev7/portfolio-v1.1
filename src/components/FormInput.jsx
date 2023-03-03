8a5015c1dfc1e1b4df9769a81c036ba9c5e5eb9d
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-03-03 15:25:52 -0600

    Added a name attribute.

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -7,7 +7,8 @@ export default function FormInput({ id, title, type, span = "", register }) {
       <input
         id={id}
         type={type}
-        className="bg-white_op_0 w-full border rounded-sm border-white_op_70 text-lg px-2 py-1"
+        name={id}
+        className="bg-op_0 w-full border rounded-sm border-white_op_70 text-lg px-2 py-1"
         {...register}
       />
     </div>
