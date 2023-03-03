b052ec154e77dbca36c6ba002518ee884d729ccf
Joel <a3518110115@alumno.uttehuacan.edu.mx>
2023-03-02 19:36:07 -0600

    Changed some styles.

--- src/components/ContentWrapper.jsx
+++ src/components/ContentWrapper.jsx
@@ -1,6 +1,6 @@
 export default function ContentWrapper({ children }) {
   return (
-    <div className="bg-gradient-to-r from-dark_blue via-dark_purple to-lighter_purple w-full text-white_op_70 font-poppins">
+    <div className="bg-gradient-to-r from-dark_blue via-dark_purple to-lighter_purple w-full text-xlight_green_op_75 font-poppins">
       {children}
     </div>
   );
